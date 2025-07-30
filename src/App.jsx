c91208495a92e2a42c967884e33acb77212f141b
matcir <matteocirilloboolean@gmail.com>
2025-07-30 18:41:35 +0200

    implemented select to filter movies and tv series with genres too

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import AppHeader from "./components/AppHeader"
 import AppMain from "./components/AppMain"
@@ -8,10 +8,45 @@ export default function App() {
   const [filteredSeries, setFilteredSeries] = useState([])
   const [filteredMovies, setFilteredMovies] = useState([])
   const [filteredAll, setFilteredAll] = useState([])
+  const [moviesGenres, setMoviesGenres] = useState([])
+  const [seriesGenres, setSeriesGenres] = useState([])
+  const [allGenres, setAllGenres] = useState([])
+  const [selectedGenre, setSelectedGenre] = useState("")
   const [searchQuery, setSearchQuery] = useState("")
+
+  const urlMoviesGenre = `https://api.themoviedb.org/3/genre/movie/list?api_key=${import.meta.env.VITE_DBMOVIE_KEY}`
+  const urlSeriesGenre = `https://api.themoviedb.org/3/genre/tv/list?api_key=${import.meta.env.VITE_DBMOVIE_KEY}`
+
+  useEffect(() => {
+    Promise.all([
+      fetch(urlMoviesGenre)
+        .then(res => res.json())
+        .then(data3 => {
+          setMoviesGenres(data3.genres)
+          return data3.genres
+        }),
+      fetch(urlSeriesGenre)
+        .then(res => res.json())
+        .then(data4 => {
+          setSeriesGenres(data4.genres)
+          return data4.genres
+        })
+    ])
+
+      .then(([moviesGenres, seriesGenres]) => {
+        const all = [...moviesGenres, ...seriesGenres]
+
+        const uniqueGenres = all.filter((genre, index, self) =>
+          index === self.findIndex(item => item.name === genre.name)
+        );
+
+        setAllGenres(uniqueGenres)
+      })
+  })
 
   function handleSubmit(e) {
     e.preventDefault()
+
     const urlMovies = `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_DBMOVIE_KEY}&query=${searchQuery}`
     const urlSeries = `https://api.themoviedb.org/3/search/tv?api_key=${import.meta.env.VITE_DBMOVIE_KEY}&query=${searchQuery}`
 
@@ -27,7 +62,7 @@ export default function App() {
         .then(data2 => {
           setFilteredSeries(data2)
           return data2.results
-        })
+        }),
     ])
 
 
@@ -39,14 +74,23 @@ export default function App() {
             original_title: serie.original_name,
             id: serie.id,
             vote_average: serie.vote_average,
-            poster_path: serie.poster_path
+            poster_path: serie.poster_path,
+            name: serie.name
           }))
+        ];
 
 
-        ]
+        const filtered = selectedGenre
+          ? all.filter(item =>
+            item.genre_ids?.includes(Number(selectedGenre))
+          )
+          : all;
 
-        setFilteredAll(all);
+
+        setFilteredAll(filtered);
       })
+
+
 
   }
 
@@ -68,7 +112,7 @@ export default function App() {
   return (
     <>
 
-      <AppHeader searchQuery={searchQuery} setSearchQuery={setSearchQuery} handleSubmit={handleSubmit} />
+      <AppHeader searchQuery={searchQuery} setSearchQuery={setSearchQuery} handleSubmit={handleSubmit} allGenres={allGenres} selectedGenre={selectedGenre} setSelectedGenre={setSelectedGenre} />
       <AppMain filteredAll={filteredAll} setFilteredAll={setFilteredAll} getRating={getRating} />
     </>
   )
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,15 +1,21 @@
-export default function AppHeader({ searchQuery, setSearchQuery, handleSubmit }) {
+export default function AppHeader({ searchQuery, setSearchQuery, handleSubmit, allGenres, setSelectedGenre }) {
     return (
         <>
             <header>
                 <div className="bg-black d-flex justify-content-between">
                     <h3 className="p-3 text-danger">BOOLFLIX</h3>
                     <form className="p-3 " onSubmit={handleSubmit}>
-                        <input type="text" placeholder="Inserisci il titolo di un film..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                        <input className="mx-3" type="text" placeholder="Inserisci il titolo di un film..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                        <select name="genres" id="genres" onChange={(e) => setSelectedGenre(e.target.value)}>
+                            <option value="">Tutti i generi</option>
+                            {allGenres.map(genre => (
+                                <option key={genre.id} value={genre.id} > {genre.name}</option>
+                            ))}
+                        </select>
                         <button type="submit" className="mx-2 btn btn-danger">Cerca</button>
                     </form>
                 </div>
-            </header>
+            </header >
         </>
     )
 }
